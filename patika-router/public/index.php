49f12eb46a7efbbaf33d890a4919d39fe0ba3615
ancyra2 <duran.talih@gmail.com>
2021-10-30 22:08:42 +0300

    Patika PHP Router Uygulaması

--- patika-router/public/index.php
+++ patika-router/public/index.php
@@ -7,11 +7,3 @@ require __DIR__ . '/../vendor/autoload.php';
 $routes = require __DIR__ . '/../route/routes.php'; 
 
 (new Router($routes))();
-
-
-
-
-
-/*foreach ($_SERVER as $k => $v){
-    echo $k . "=>" . "$v" . "<br>";
-}*/
